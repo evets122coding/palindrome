6d5e12224253fc80a35533f911f7d9c891571278
Steven Lufrano <evets122coding@gmail.com>
2021-04-29 20:56:51 -0400

    Initialize repository

---
+++ index.js
@@ -0,0 +1,23 @@
+// Add `reverse` to all strings.
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palaindrome testing
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
+  // Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+Array.prototype.last = function last() {
+  return this.slice(-1);
+}
